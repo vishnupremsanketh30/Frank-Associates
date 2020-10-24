2064d4fce9e3c478421a84db278e794cf6e34975
vishnu prem sanketh galla <vishnu.premsanketh@outlook.com>
2020-10-24 22:22:59 +0800

    first deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig  : {
+    apiKey: "AIzaSyBZL_kmmXKkSGV_BuHoXhg9TKxeRZs4uhM",
+    authDomain: "fir-1e1f5.firebaseapp.com",
+    databaseURL: "https://fir-1e1f5.firebaseio.com",
+    projectId: "fir-1e1f5",
+    storageBucket: "fir-1e1f5.appspot.com",
+    messagingSenderId: "813157824357",
+    appId: "1:813157824357:web:9ce43ab1198ba6699e938c"
+  }
 };
